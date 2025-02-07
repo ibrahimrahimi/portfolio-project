02b0026eb66f87764d615d8689f09bade094d0f2
ibrahim <ebrahimrahimilaziri@gmail.com>
2025-02-07 11:35:48 +0100

    - Add migrations, user basic password verification and hasing.

--- backend/app/models.py
+++ backend/app/models.py
@@ -11,7 +11,7 @@ class Blog(Base):
 
 
 class User(Base):
-    __tablename__ = "Users"
+    __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, nullable=False)
