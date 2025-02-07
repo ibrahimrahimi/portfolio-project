627fc36ab107dec9ada1bad4087b177728d616a9
ibrahim <ebrahimrahimilaziri@gmail.com>
2025-02-07 19:01:09 +0100

    - Add protected routes (dashboard,profile).

---
+++ backend/app/routes/protected.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter, Depends
+from app.auth import get_current_user
+
+router = APIRouter(prefix="/protected", tags=["Protected Routes"])
+
+
+@router.get("/dashborad")
+async def protected_dashboard(current_user: str = Depends(get_current_user)):
+    """Only authenticated users can access this"""
+    return {"message": "Welcome to your dashboard", "user": current_user}
+
+
+@router.get("/profile")
+async def user_profile(current_user: str = Depends(get_current_user)):
+    """User profile, accessible only by logged-in users"""
+    return {"email":current_user, "message":"This is your profile!"}
